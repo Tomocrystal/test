3c69bed55216f2f51636c490748ee87f08f6851b
Tomocrystal <tomocrystal@mail.ustc.edu.cn>
2024-03-20 11:50:09 +0800

    1

---
+++ hello.py
@@ -0,0 +1,10 @@
+''' 
+ @ Description: 
+ @ FilePath: /test/hello.py
+ @ Author: plzeng17b@imr.ac.cn
+ @ Date: 2024-03-20 11:17:37
+ @ LastEditTime: 2024-03-20 11:26:53
+ @ Copyright (c) 2024 by IMR DuGroup, All Rights Reserved. 
+'''
+print('Hello World!')
+print('Hello World!')
